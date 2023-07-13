4fd1b603bd0ffc089be2d844c05ce712adc5d567
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-07-13 17:44:35 +0300

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,17 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
       },
-    },
-  },
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: env('NETLIFY_ACCESS_TOKEN'),
     },
   },
 });
